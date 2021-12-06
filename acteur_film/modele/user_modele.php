5b7dd19db2725560a24bd58cf19ac8066a7d48a7
Sidem <antoine.adjamidis@gmail.com>
2021-12-06 19:57:15 +0100

    Finalisation

--- acteur_film/modele/user_modele.php
+++ acteur_film/modele/user_modele.php
@@ -1,4 +1,5 @@
 <?php
+	require_once('./controler/user_controler.php');
     class user extends manager {
 		
         public function seconnecter($pseudo, $mdp){
@@ -26,6 +27,7 @@
 				if ($nb == 0){
 					$verif = $this->verif_mdp($mdp);
 					if ($verif == true){
+						$objet = 
 						$querydeux = $this->bdd->prepare("INSERT into `utilisateur` (pseudo, mdp) VALUES (:pseudo, :mdp)");
 						$testdeux = $querydeux ->execute(array(':pseudo' => $pseudo, ':mdp' => hash('sha256', $mdp))); 
 						header("location: login.php");
@@ -39,30 +41,7 @@
 		}
 
 		public function co_deco($todo){
-			if (isset($_SESSION['pseudo'])){
-                switch ($todo){
-                    case '1':
-                        return "Bonjour " . $_SESSION['pseudo'] . " ! Vous etes un " . $_SESSION['type'];
-                        break;
-					case '2':
-						return "<a href='logout.php'>Se déconnecter</a></br>";
-						break;
-                    default:
-                        break;
-                }
-			} else {
-                switch ($todo) {
-                    case '1':
-                        return "Hésite pas à te co bg";
-                        break;
-					case '2':
-						return "<a href='login.php'>Se connecter</a></br>";
-						break;
-                    default:
-                        break;
-                }
-				
-			}
+			
 		}
 
 		public function verif_mdp($mdp){
